4145e76649caa43d22ccd644b938c76dc7579953
codemickeycode <codemickeycode@gmail.com>
2013-10-06 03:23:15 +0800

    Added Django CMS url patterns

--- pythonph/pythonph/urls.py
+++ pythonph/pythonph/urls.py
@@ -1,17 +1,20 @@
 from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
+from django.conf import settings
+
+
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'pythonph.views.home', name='home'),
-    # url(r'^pythonph/', include('pythonph.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+urlpatterns = i18n_patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('cms.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns = patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+        url(r'', include('django.contrib.staticfiles.urls')),
+    ) + urlpatterns
+
