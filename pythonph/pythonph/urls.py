39188cc5033d07342a3abdce59e43f0e0d17018f
codemickeycode <codemickeycode@gmail.com>
2013-10-06 21:11:18 +0800

    Temporarily disabled translation in urls.py

--- pythonph/pythonph/urls.py
+++ pythonph/pythonph/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import patterns, include, url
-from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.conf import settings
 
 
 admin.autodiscover()
 
-urlpatterns = i18n_patterns('',
+urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('cms.urls')),
 )
